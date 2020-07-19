c5f3655412428d84724bad2d554b6d6a8e9b3be1
khanempire <mdaamirkhan7186@gmail.com>
2020-07-19 14:00:10 +0530

    Updates

---
+++ precache-manifest.f726de93698e72466c39f147c97db060.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85e65a483908f8c646b898a26a262e85",
+    "url": "/calculator-react-app/index.html"
+  },
+  {
+    "revision": "ac2b74bec4d7e1f8df53",
+    "url": "/calculator-react-app/static/js/2.80404aad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator-react-app/static/js/2.80404aad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ace989e7f2d0d6642df",
+    "url": "/calculator-react-app/static/js/main.1aa34666.chunk.js"
+  },
+  {
+    "revision": "c51b9b4d64112b6af060",
+    "url": "/calculator-react-app/static/js/runtime-main.bf3fc25a.js"
+  }
+]);
